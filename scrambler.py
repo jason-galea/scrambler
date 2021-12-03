bfee99599e7283f7719bb3760d85539a2ef44847
Jason Galea <jasonpgalea@outlook.com.au>
2021-12-03 19:46:23 +1100

    DONE. All features added, all bugs squashed

--- scrambler.py
+++ scrambler.py
@@ -1,43 +1,36 @@
 from random import randint
-from sys import argv
 import argparse
 
 # Constants
 MOVES = ["U", "D", "L", "R", "F", "B"]
-OPPOSITES = ["D", "U", "R", "L", "B", "F"] # Wasteful, but simplifies logic/data structure
+OPPOSITES = ["D", "U", "R", "L", "B", "F"] # Wasteful, but fast & simple
 
-def main():
+def main(args):
     # Local variables
-    prevMove1 = ""
-    prevMove2 = ""
-    showWarnings = False
-    if showWarnings:
+    prevMove1 = prevMove2 = ""
+    if args.v:
         warningBuffer = []
+    n = args.l
 
-    if (True): # TODO: Check if n is defined?
-        try:
-            n = int(input("\nEnter the scramble length: "))
-        except:
-            exit("ERROR: Please enter a numerical value\n")
-
-
+    # Main loop
     for _ in range(n):
         while True:
-            r1 = randint(0, 5)
+            r1 = randint(0, 5) # Necessary for checking redundancy 
             move = MOVES[r1]
 
-            # Ensure moves are not repeated
+            # Ensure moves are not repeated (U U)
             if (move is prevMove1):
-                if showWarnings:
-                    warningBuffer.append(f"WARNING: Duplicate move found: {move}")
+                if args.v: # Not necessary, but saves memory when not running verbosely
+                    warningBuffer.append(f"WARNING: Regenerating duplicate move: {move}")
                 continue
 
-            # Ensure moves are not redundant, such as U D U'
+            # Ensure moves are not redundant (U D U)
             if (move is prevMove2) and (prevMove1 is OPPOSITES[r1]):
-                if showWarnings:
-                    warningBuffer.append(f"WARNING: Redundant moves found: {move} {prevMove1} {prevMove2}")
+                if args.v:
+                    warningBuffer.append(f"WARNING: Regenerating redundant moves: {move} {prevMove1} {prevMove2}")
                 continue
             
+            # Success, store move
             prevMove2 = prevMove1
             prevMove1 = move
             break
@@ -49,23 +42,23 @@ def main():
         elif (r2 == 1):
             move += "'"
 
+        # Print
         print(f"{move} ", end ="")
-    print("\n")
+    print()
 
-    if showWarnings:
+    # Print warnings if verbose
+    if args.v:
         for warning in warningBuffer:
             print(f"{warning}")
-        print()
 
     exit(1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("")
-    #
-    #
-    #
-
+    parser.add_argument("-l", type=int, default=18,
+        help="Give length as argument, instead of during execution")
+    parser.add_argument("-v", action="store_true", # Eg. default=False
+        help="Show warnings when invalid moves are regenerated")
     args = parser.parse_args()
-    
-    main(args.name)
+
+    main(args)
